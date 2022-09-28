d5c13b521e112eff291a3dd8697c7e814595f103
AndyLu <laserlue@gmail.com>
2022-09-28 11:31:20 +0800

    Convert temperatures between Fahrenheit and Celsius

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,8 +1,22 @@
 fn main() {
-    let result = say_hello();
-    println!("Hello, world! {result}");
+    // for number in (1..4).rev(){
+    //     println!("{number}")
+    // }
+    //
+    // let mut a = [1,2,30,50,350];
+    // a.reverse();
+    // for element in a {
+    //     println!("the value is: {element}")
+    // }
+    
+    // let result = say_hello();
+    // println!("Hello, world! {result}");
+
+    let f:f64 = 100.0;
+    let c: f64 = (f-32.0) / 1.80; 
+    print!("Degree: {c}")
 }
 
-fn say_hello()-> String {
-    return "eeee".to_string();
-}
+// fn say_hello()-> String {
+//     return "eeee".to_string();
+// }
