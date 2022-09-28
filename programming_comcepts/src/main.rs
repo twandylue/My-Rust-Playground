be89feeb0a4949e39abd50dda754bfdb717aa003
AndyLu <laserlue@gmail.com>
2022-09-28 13:50:22 +0800

    Generate Fibonacci Sequence

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,22 +1,16 @@
 fn main() {
-    // for number in (1..4).rev(){
-    //     println!("{number}")
-    // }
-    //
-    // let mut a = [1,2,30,50,350];
-    // a.reverse();
-    // for element in a {
-    //     println!("the value is: {element}")
-    // }
-    
-    // let result = say_hello();
-    // println!("Hello, world! {result}");
-
-    let f:f64 = 100.0;
-    let c: f64 = (f-32.0) / 1.80; 
-    print!("Degree: {c}")
+    let target = 20;
+    generate_fibonacci_sequence(0, 1, target);
 }
 
-// fn say_hello()-> String {
-//     return "eeee".to_string();
-// }
+fn generate_fibonacci_sequence(pre_number: u32, mut cur_number: u32, countdown: u32) {
+
+    if countdown == 0 {return}
+
+    let temp = cur_number;
+    cur_number = pre_number + cur_number;
+    println!("{countdown}th Number: {cur_number}");
+
+    generate_fibonacci_sequence(temp, cur_number, countdown - 1);
+}
+
