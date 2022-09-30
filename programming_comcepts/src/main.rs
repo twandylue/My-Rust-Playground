e9f07f99de3ff3849a675f2a72a06087474bda18
AndyLu <laserlue@gmail.com>
2022-09-30 18:56:23 +0800

    understand ownership

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,16 +1,36 @@
 fn main() {
-    let target = 20;
-    generate_fibonacci_sequence(0, 1, target);
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    let rect2 = Rectangle {
+        width: 35,
+        height: 55,
+    };
+
+    let rect3 = Rectangle {
+        width: 20,
+        height: 30,
+    };
+
+    println!("{:#?}", rect1.area());
+    println!("{:#?}", rect1.can_hold(&rect2));
+    println!("{:#?}", rect1.can_hold(&rect3));
 }
 
-fn generate_fibonacci_sequence(pre_number: u32, mut cur_number: u32, countdown: u32) {
-
-    if countdown == 0 {return}
-
-    let temp = cur_number;
-    cur_number = pre_number + cur_number;
-    println!("{countdown}th Number: {cur_number}");
-
-    generate_fibonacci_sequence(temp, cur_number, countdown - 1);
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
