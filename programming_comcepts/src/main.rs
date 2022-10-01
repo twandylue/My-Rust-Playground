331703128061d1fe907b35ab1f38468b30dec180
AndyLu <laserlue@gmail.com>
2022-10-01 12:33:18 +0800

    match

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,36 +1,20 @@
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
-
-    let rect2 = Rectangle {
-        width: 35,
-        height: 55,
-    };
-
-    let rect3 = Rectangle {
-        width: 20,
-        height: 30,
-    };
-
-    println!("{:#?}", rect1.area());
-    println!("{:#?}", rect1.can_hold(&rect2));
-    println!("{:#?}", rect1.can_hold(&rect3));
+    let x = value(Coin::Nickel);
+    println!("{:#?}", x);
 }
 
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
 }
 
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
+fn value(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1, 
+        Coin::Dime => 10,
+        Coin::Nickel => 5,
+        Coin::Quarter => 25,
     }
 }
