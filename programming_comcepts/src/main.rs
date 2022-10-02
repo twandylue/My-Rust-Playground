1a8dcc53049f681110fc49cb66f8f2e3416cade8
AndyLu <laserlue@gmail.com>
2022-10-02 13:12:23 +0800

    Enum in vec

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,26 +1,16 @@
 fn main() {
-    let number_1 = generate_fibonacci_sequence(30);
-    let number_2 = generate_fibonacci_sequence_recursion(30);
-    assert_eq!(number_1, number_2);
-    println!("Number_1: {}, Number_2: {}", number_1, number_2);
+    let row = vec![
+        Cell::Int(32), 
+        Cell::Float(3.23), 
+        Cell::Text(String::from("blue"))
+    ];
+
+    println!("{:?}", row)
 }
 
-fn generate_fibonacci_sequence(depth: i64) -> i64 {
-    let mut a = 0;
-    let mut b = 1;
-
-    for _ in 0..depth {
-        let temp = b;
-        b = a + b;
-        a = temp;
-    };
-
-    return a
-}
-
-fn generate_fibonacci_sequence_recursion(depth: i64) -> i64 {
-    if depth <= 1 {depth}
-    else {
-        generate_fibonacci_sequence_recursion(depth-1) + generate_fibonacci_sequence_recursion(depth-2)
-    }
+#[derive(Debug)]
+enum Cell {
+    Int(i32),
+    Float(f64),
+    Text(String),
 }
