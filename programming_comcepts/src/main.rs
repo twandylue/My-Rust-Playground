4fa66210deb7e932e0ed13c30dcf6bf41955cc06
AndyLu <laserlue@gmail.com>
2022-10-05 22:33:52 +0800

    find the mode

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,16 +1,21 @@
 fn main() {
-    let row = vec![
-        Cell::Int(32), 
-        Cell::Float(3.23), 
-        Cell::Text(String::from("blue"))
-    ];
+    use std::collections::HashMap;
+    let mut map = HashMap::new();
 
-    println!("{:?}", row)
-}
+    let v = vec![1, 2, 3, 4, 4, 4, 4, 5, 5, 2, 2, 3, 3, 4];
 
-#[derive(Debug)]
-enum Cell {
-    Int(i32),
-    Float(f64),
-    Text(String),
+    for number in v {
+        let count = map.entry(number).or_insert(0);
+        *count += 1;
+    }
+
+    let mut max_count: u32 = 0;
+    for (_, v) in &map {
+        if max_count < *v {
+            max_count = *v
+        }
+    }
+
+    println!("{:#?}", map);
+    println!("{:#?}", max_count)
 }
