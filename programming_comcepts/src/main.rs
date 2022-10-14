8098a936a22801205cb859b2cd464e04e513652b
AndyLu <laserlue@gmail.com>
2022-10-14 19:48:40 +0800

    practice of traits

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,55 +1,40 @@
-use std::collections::HashMap;
-
 fn main() {
-    let data = vec![
-        ("zeo", "Engineering"),
-        ("andy", "Engineering"),
-        ("leo", "Sales"),
-        ("amy", "Sales"),
-        ("rick", "Engineering"),
-    ];
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
 
-    let map = add_people(data);
-    let department = "Sales";
-    println!("Total people: {:#?}", retrive_all_people(&map));
-    println!("---");
-    println!("{}: {:#?}", department, retrive_people(department, &map));
-    println!("---");
-    let department = "Engineering";
-    println!("{}: {:#?}", department, retrive_people("Engineering", &map));
+    println!("1 new tweet: {}", tweet.summarize())
 }
 
-fn add_people(data: Vec<(&str, &str)>) -> HashMap<String, Vec<String>> {
-    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
 
-    for (name, department) in data {
-        let list = map.entry(String::from(department)).or_insert(Vec::new());
-        list.push(String::from(name));
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
-
-    return map;
 }
 
-fn retrive_all_people(map: &HashMap<String, Vec<String>>) -> Vec<String> {
-    let mut total: Vec<String> = Vec::new();
-    for (_, list) in map.into_iter() {
-        for name in list {
-            total.push(String::from(name));
-        }
-    }
-
-    total.sort();
-    return total;
-}
-
-fn retrive_people(department: &str, map: &HashMap<String, Vec<String>>) -> Vec<String> {
-    let result = map.get(department);
-    match result {
-        None => panic!(),
-        Some(list) => {
-            let mut ans = list.to_vec();
-            ans.sort();
-            ans
-        }
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
     }
 }
