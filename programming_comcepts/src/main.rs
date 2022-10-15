f125f0d679f205da4f35e883a37a5a3fa75ebbf3
AndyLu <laserlue@gmail.com>
2022-10-15 16:31:11 +0800

    practice of traits

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -9,8 +9,21 @@ fn main() {
     println!("1 new tweet: {}", tweet.summarize())
 }
 
+// sugar syntax
+// pub fn notify(item: &impl Summary) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+
 pub trait Summary {
-    fn summarize(&self) -> String;
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
+    fn summarize_author(&self) -> String;
 }
 
 pub struct NewsArticle {
@@ -31,10 +44,27 @@ impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.author)
+    }
 }
 
 impl Summary for Tweet {
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
     }
 }
