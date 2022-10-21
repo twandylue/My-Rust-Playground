890afeaeae82547fd1af77509e4312b3a51ed4d3
AndyLu <laserlue@gmail.com>
2022-10-22 07:13:21 +0800

    practice for Lifetimes

--- programming_comcepts/src/main.rs
+++ programming_comcepts/src/main.rs
@@ -1,70 +1,43 @@
+use std::fmt::Display;
+
 fn main() {
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
+    let novel = String::from("Call me Andy. Some year ago...");
+    let test = String::from("test");
 
-    println!("1 new tweet: {}", tweet.summarize())
+    println!(
+        "{:#?}",
+        longest_with_an_announcement(novel.as_str(), test.as_str(), "String")
+    )
 }
 
-// sugar syntax
-// pub fn notify(item: &impl Summary) {
-//     println!("Breaking news! {}", item.summarize());
-// }
-
-pub fn notify<T: Summary>(item: &T) {
-    println!("Breaking news! {}", item.summarize());
-}
-
-pub trait Summary {
-    fn summarize(&self) -> String {
-        format!("(Read more from {}...)", self.summarize_author())
-    }
-
-    fn summarize_author(&self) -> String;
-}
-
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
-
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
-    }
-
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.author)
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement: {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
 
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
     }
 
-    fn summarize_author(&self) -> String {
-        format!("@{}", self.username)
-    }
+    &s[..]
 }
 
-fn returns_summarizable() -> impl Summary {
-    Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
