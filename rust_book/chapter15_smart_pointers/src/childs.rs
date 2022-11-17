72a354c293220e7617a5790233d72fe73dbfa468
AndyLu <laserlue@gmail.com>
2022-11-17 23:21:22 +0800

    update chapter15_smart_pointers

--- rust_book/chapter15_smart_pointers/src/childs.rs
+++ rust_book/chapter15_smart_pointers/src/childs.rs
@@ -2,3 +2,4 @@ pub mod prac_Box;
 pub mod prac_Rc;
 pub mod prac_RefCell;
 pub mod prac_ref_cyc_leak;
+pub mod prac_ref_cyc_leak2;
--- rust_book/chapter15_smart_pointers/src/main.rs
+++ rust_book/chapter15_smart_pointers/src/main.rs
@@ -1,11 +1,15 @@
 use crate::childs::prac_Box;
 use crate::childs::prac_Rc;
 use crate::childs::prac_RefCell;
+use crate::childs::prac_ref_cyc_leak;
+use crate::childs::prac_ref_cyc_leak2;
 
 mod childs;
 
 fn main() {
     // prac_Box::run();
     // prac_Rc::run();
-    prac_RefCell::run();
+    // prac_RefCell::run();
+    // prac_ref_cyc_leak::run();
+    prac_ref_cyc_leak2::run();
 }
