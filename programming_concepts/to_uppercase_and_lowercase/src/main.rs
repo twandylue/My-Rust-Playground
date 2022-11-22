34b2aabb17c969e8099bb9ae033f94a5d05abe5c
AndyLu <laserlue@gmail.com>
2022-11-22 21:07:55 +0800

    update to_uppercase_and_lowercase

--- programming_concepts/to_uppercase_and_lowercase/src/main.rs
+++ programming_concepts/to_uppercase_and_lowercase/src/main.rs
@@ -5,6 +5,20 @@ fn main() {
     let mut a: Vec<char> = t.chars().collect();
     a[0] = a[0].to_uppercase().nth(0).unwrap();
     t = a.iter().collect();
-
+    assert_eq!(t, String::from("Test"));
     println!("t: {:?}", t);
+
+    let t2 = self::convert_to_capital("test");
+    assert_eq!(t2, String::from("Test"));
+    println!("t2: {:?}", t2);
 }
+
+fn convert_to_capital(word: &str) -> String {
+    let mut c = word.chars();
+
+    match c.next() {
+        None => String::new(),
+        Some(e) => e.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
