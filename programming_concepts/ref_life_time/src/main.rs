2e845620076e4888c67c868712508983bdfc63fc
AndyLu <laserlue@gmail.com>
2022-11-24 07:56:11 +0800

    update ref_life_time

--- programming_concepts/ref_life_time/src/main.rs
+++ programming_concepts/ref_life_time/src/main.rs
@@ -1,20 +1,45 @@
 use std::fmt::Display;
 
 fn main() {
-    let novel = String::from("Call me Andy. Some year ago...");
+    let novel = String::from("My name is Andy");
     let test = String::from("test");
+    let ret = self::longest_with_an_announcement(novel.as_str(), test.as_str(), "String");
 
-    println!(
-        "{:#?}",
-        longest_with_an_announcement(novel.as_str(), test.as_str(), "String")
-    )
+    assert_eq!(String::from("My name is Andy"), ret);
+
+    let mut bytes = ByteIter { remainder: b"1" };
+    let byte1 = bytes.next();
+    let byte2 = bytes.next();
+    if byte1 == byte2 {}
+}
+
+struct ByteIter<'remainder> {
+    remainder: &'remainder [u8],
+}
+
+impl<'remainder> ByteIter<'remainder> {
+    // ERROR because of lifetime annotation
+    // fn next<'mut_self>(&'mut_self mut self) -> Option<&'mut_self u8> {
+    fn next<'mut_self>(&'mut_self mut self) -> Option<&'remainder u8> {
+        if self.remainder.is_empty() {
+            None
+        } else {
+            let byte = &self.remainder[0];
+            self.remainder = &self.remainder[1..];
+            Some(byte)
+        }
+    }
 }
 
 fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
 where
     T: Display,
 {
-    println!("Announcement: {}", ann);
+    assert_eq!(
+        String::from("Announcement: String"),
+        format!("Announcement: {}", ann)
+    );
+
     if x.len() > y.len() {
         x
     } else {
